ba5f1edd3c516b3cbf224abc63af7761a7584f2e
RaspberryProgramming <figueroa0609@protonmail.com>
2021-04-12 16:08:35 -0400

    Added comments to js code

--- public/js/manager.js
+++ public/js/manager.js
@@ -1,90 +1,144 @@
 function preview() {
-    let selected = document.getElementsByClassName("selected");
-    for (i in selected) {
-        if (selected[i].classList) {
-            let path = window.location.pathname;
-            path = path.split("/download");
-            path.shift();
-            window.location.href = "/preview" + path.join("/") + selected[i].id;
-        }
-    }
-}
+    /**
+     * preview: used when a file is selected, and the user wants to open a preview of the file
+     */
+    let selected = document.getElementsByClassName("selected"); // Grab all files that have been selected
 
-function edit() {
-    let selected = document.getElementsByClassName("selected");
-    for (i in selected) {
-        if (selected[i].classList) {
-            let path = window.location.pathname;
-            path = path.split("/download");
-            path.shift();
-            window.location.href = "/editor" + path.join("/") + selected[i].id;
-                
-        }
-    }
-}
+    for (i in selected) { // Iterate through all of the selected items
 
-function download() {
-    let selected = document.getElementsByClassName("selected");
-    for (i in selected) {
-        if (selected[i].classList) {
-            let path = window.location.pathname;
+        if (selected[i].classList.contains("selected")) { // Verify that this item actually is selected
 
-            window.location.href = path + selected[i].id;
-        }
-    }
-}
+            let path = window.location.pathname; // Grab the page's path
+            path = path.split("/download"); // Separate "/download" from the path so we can recompile with the proper initial path
+            path.shift(); // Delete the first element
 
-function deleteFile() {
-    let selected = document.getElementsByClassName("selected");
-    for (i in selected) {
-        if (selected[i].classList) {
-            let path = window.location.pathname;
-            path = path.split("/download");
-            path.shift();
+            // Fix the path if we're at the root of the user's storage
             if (path.length <= 1) {
                 path = "/"
             } else {
                 path = path.join("/")
             }
 
-            window.location.href = "/delete" + path + selected[i].id;
+            window.location.href = "/preview" + path + selected[i].id; // append /preview to the begining with file id, and redirect to the download
+        }
+    }
+}
+
+function edit() {
+    /**
+     * edit: used when a file is selected, and the user wants to open the file editor with that file.
+     *       (This does not mean that it will run in the editor)
+     */
+    let selected = document.getElementsByClassName("selected"); // Grab all files that have been selected
+
+    for (i in selected) { // Iterate through all of the selected items
+
+        if (selected[i].classList.contains("selected")) { // Verify that this item actually is selected
+
+            let path = window.location.pathname; // Grab the page's path
+            path = path.split("/download"); // Separate "/download" from the path so we can recompile with the proper initial path
+            path.shift(); // Delete the first element
+
+            // Fix the path if we're at the root of the user's storage
+            if (path.length <= 1) {
+                path = "/"
+            } else {
+                path = path.join("/")
+            }
+
+            window.location.href = "/editor" + path + selected[i].id; // append /editor to the begining with file id, and redirect to the download
+                
+        }
+    }
+}
+
+function download() {
+    /**
+     * download: used when a file is selected and the user wants to download the file
+     */
+    let selected = document.getElementsByClassName("selected"); // Grab all files that have been selected
+
+    for (i in selected) { // Iterate through all of the selected items
+
+        if (selected[i].classList.contains("selected")) { // Verify that this item actually is selected
+
+            let path = window.location.pathname + "/"; // Grab the page's path
+            
+            window.location.href = path + selected[i].id; // Append the file's id so it will initiate a download, and redirect to the download
+        }
+    }
+}
+
+function deleteFile() {
+    /**
+     * deleteFile: used when a file is selected and the user wants to delete it
+     */
+    let selected = document.getElementsByClassName("selected"); // Grab all files that have been selected
+
+    for (i in selected) { // Iterate through all of the selected items
+
+        if (selected[i].classList.contains("selected")) { // Verify that this item actually is selected
+
+            let path = window.location.pathname; // Grab the page's path
+
+            path = path.split("/download");// Separate "/download" from the path so we can recompile with the proper initial path
+            path.shift(); // Delete the first element
+
+            // Fix the path if we're at the root of the user's storage
+            if (path.length <= 1) {
+                path = "/"
+            } else {
+                path = path.join("/")
+            }
+
+            window.location.href = "/delete" + path + selected[i].id; // append /delete to the begining with file id, and redirect to the download
         }
     }
 }
 
 function select(id, path) {
-    let selected = document.getElementsByClassName("selected");
-    for (i in selected) {
-        if (selected[i].classList) {
-            selected[i].classList.remove("selected");
-        }
-    }
-    if (!document.getElementById(id).classList.contains("selected")) {
-        document.getElementById(id).classList = "selected " + document.getElementById(id).classList;
+    /**
+     * select: used when a file is clicked in order to select/deselect the file
+     */
+    let selected = document.getElementsByClassName("selected"); // Collect all elements on the page that are selected
+
+    if (id in selected){ // check if the current item is already selected
+
+        selected[id].classList.remove("selected"); // Deselect the item if its already selected
+        selected[id].classList.add
+
+    } else {
+        // Otherwise, add selected to it's classList
+
+        document.getElementById(id).classList.add("selected");
+
     }
 
 }
 
 function search(searchTerm) {
-    //let files = document.getElementById('files');
+    /**
+     * search: used to search using a specific term
+     */
+    // Get all files and folders
     let files = document.getElementsByClassName("file");
     let folders = document.getElementsByClassName("folder");
 
-    for (let i = 0; i < files.length; i++) {
+    for (let i = 0; i < files.length; i++) { // iterate through each file
         
-        if (files[i].id.includes(searchTerm)){
+        if (files[i].id.includes(searchTerm)){ // if the filename includes the search term, it will be enabled
             files[i].style.display = "";
-        } else {
+        } else { // otherwise the file will be disabled and hidden
             files[i].style.display = "none";
         }
 
     }
 
-    for (let i = 0; i < folders.length; i++) {
+    for (let i = 0; i < folders.length; i++) { // iterate through each file
         
-        if (folders[i].id.includes(searchTerm)){
+        if (folders[i].id.includes(searchTerm)){ // if the folder name includes the search term, it will be enabled
             folders[i].style.display = "";
-        } else {
+        } else { // otherwise it will be disabled and hidden
             folders[i].style.display = "none";
         }
 
@@ -92,48 +146,70 @@ function search(searchTerm) {
 }
 
 document.getElementById("tooltoggle").onclick = function () {
-    let toolset = document.getElementById("toolset");
-    if (toolset.style.display === "flex") {
-        toolset.style.display = "none";
+    /**
+     * Used when on mobile. The tooltoggle button will enable or disable the tool menu so the user can work with the
+     * files that they've selected or if they want to log out.
+     */
+    let toolset = document.getElementById("toolset"); // grab the toolset element
+    if (toolset.style.display === "flex") { // if the toolset is enabled
+        toolset.style.display = "none"; // disable it by setting it's display style to none
 
     } else {
-        toolset.style.display = "flex";
+        toolset.style.display = "flex"; // enable the toolset by setting it's display style to flex
     }
-    console.log(toolset);
+    
 }
+
 document.getElementById("fileupload").onchange = function () {
+    /**
+     * Used to upload files to the current folder. The file will immediately be submitted and uploaded. There currently
+     * is no progress bar to let you know how far along the file is.
+     */
     document.getElementById("upload-form").submit();
 };
 
 (function () {
+    /**
+     * Used to display progress of the lazyloader
+     */
+
     function logElementEvent(eventName, element) {
+        /**
+         * logElementEvent: runs when an event occurs to log the occurence
+         */
+        // Log the event
         console.log(
-            Date.now(),
-            eventName,
-            element.getAttribute("data-src")
+            Date.now(), // add the date
+            eventName, // add event name
+            element.getAttribute("data-src") // add element image or placeholder
         );
     }
 
     var callback_enter = function (element) {
         logElementEvent("🔑 ENTERED", element);
     };
+
     var callback_exit = function (element) {
         logElementEvent("🚪 EXITED", element);
     };
+
     var callback_reveal = function (element) {
         logElementEvent("👁️ REVEALED", element);
     };
+
     var callback_loaded = function (element) {
         element.style['visibility'] = "visible";
 
         logElementEvent("👍 LOADED", element);
     };
+
     var callback_error = function (element) {
 
         logElementEvent("💀 ERROR", element);
         element.src =
             "https://via.placeholder.com/440x560/?text=Error+Placeholder";
     };
+
     var callback_finish = function () {
         logElementEvent("✔️ FINISHED", document.documentElement);
     };
